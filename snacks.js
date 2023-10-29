f5807c320412ccaeeba732e883d53e078ab348e3
ericleirosario <ericleirosario@gmail.com>
2023-10-29 17:28:27 -0300

    Inclução do type dos snacks

--- snacks.js
+++ snacks.js
@@ -1,8 +1,8 @@
 const menuSnacks = [
-    { name: 'Coxinha Gourmet', description:'A coxinha mais saborosa de frango com catupiry', price: 10, vegan: false, src: './img/coxinha.jpg' },
-    { name: 'Mousse de Morango', description: 'Uma sobremesa muito saborosa para o após-refeição',price: 9.50, vegan: false, src: './img/mousse.png' },
-    { name: 'Hamburger Artesanal', description:'Execelente hamburger artesanal feito com a melhor carne angus', price: 25, vegan: false, src: './img/hamburger-artesanal.jpg' },
-    { name: 'Pizza Vegana', price: 55, description:'Esse é um dos sabores de pizza mais pedidos por quem é vegano', vegan: true, src: './img/pizza.png' },
-    { name: 'Limonada Suave', price: 5, description:'A limonada é um suco refrescante e ideal para dias de intenso calor', vegan: false, src: './img/limonada.jpg' },
-    { name: 'Suco de Laranja', price: 5, description:'É um suco bastante popular e que vai bem com qualquer lanche', vegan: false, src: './img/suco-de-laranja.jpg' },
+    { name: 'Coxinha Gourmet', description:'A coxinha mais saborosa de frango com catupiry', type: 'Lanche', price: 10, vegan: false, src: './img/coxinha.jpg' },
+    { name: 'Mousse de Morango', description: 'Uma sobremesa muito saborosa para o após-refeição', type: 'Sobremesa', price: 9.50, vegan: false, src: './img/mousse.png' },
+    { name: 'Hamburger Artesanal', description:'Execelente hamburger artesanal feito com a melhor carne angus', type: 'Lanche', price: 25, vegan: false, src: './img/hamburger-artesanal.jpg' },
+    { name: 'Pizza Vegana', description:'Esse é um dos sabores de pizza mais pedidos por quem é vegano', type: 'Massas', price: 55, vegan: true, src: './img/pizza.png' },
+    { name: 'Limonada Suave', description:'A limonada é um suco refrescante e ideal para dias de intenso calor', type: 'Bebida', price: 5, vegan: false, src: './img/limonada.jpg' },
+    { name: 'Suco de Laranja', description:'É um suco bastante popular e que vai bem com qualquer lanche', type: 'Bebida', price: 5, vegan: false, src: './img/suco-de-laranja.jpg' },
 ];
